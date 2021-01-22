2bdd51cfe8f8702372bd56c857a0dd1c79846fe1
yvonneonu <yvonneonuorah@gmail.com>
2021-01-22 10:06:08 +0100

    login class

---
+++ app/src/main/java/com/mystic/movieshub/AccSharedPref.java
@@ -0,0 +1,28 @@
+package com.mystic.movieshub;
+
+import android.content.Context;
+import android.preference.PreferenceManager;
+
+public class AccSharedPref {
+    private static final String PREF_EMAIL = "";
+    private static final String FIRST_TIME = "FirstTime";
+
+    public static String getStoredEmail(Context context){
+        return PreferenceManager.getDefaultSharedPreferences(context)
+                .getString(PREF_EMAIL, null);
+    }
+    public static void setStoredEmail(Context context, String query){
+        PreferenceManager.getDefaultSharedPreferences(context)
+                .edit().putString(PREF_EMAIL, query)
+                .apply();
+    }
+    public static void setUserState(Context context, boolean state){
+        PreferenceManager.getDefaultSharedPreferences(context)
+                .edit().putBoolean(FIRST_TIME, state)
+                .apply();
+    }
+    public  static boolean getUserState(Context context){
+        return PreferenceManager.getDefaultSharedPreferences(context)
+                .getBoolean(FIRST_TIME, true);
+    }
+}
