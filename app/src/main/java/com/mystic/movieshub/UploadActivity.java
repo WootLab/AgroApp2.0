a637600f3bec47e7149dc2817d252166c5b33601
mystic001 <bamideleomonayin@gmail.com>
2021-02-17 21:34:30 +0000

    designing spinner

--- app/src/main/java/com/mystic/movieshub/UploadActivity.java
+++ app/src/main/java/com/mystic/movieshub/UploadActivity.java
@@ -40,7 +40,6 @@ public class UploadActivity extends AppCompatActivity {
     private EditText edtTitle,edtDesc,location;
     private TextView fileName;
     private AgroAppRepo agroAppRepo ;
-    private Button uploadBtn, cancelBtn,choBtn;
     private static final  int IMAGE_VALUE = 1;
     private Uri mImageUri;
     private ProgressBar bar;
@@ -57,14 +56,9 @@ public class UploadActivity extends AppCompatActivity {
         Objects.requireNonNull(getSupportActionBar()).setTitle("Upload Your Product");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                finish();
-            }
-        });
+        toolbar.setNavigationOnClickListener(view -> finish());
         mStorageRef = FirebaseStorage.getInstance().getReference("PRODUCTS");
-        choBtn = findViewById(R.id.choose);
+        Button choBtn = findViewById(R.id.choose);
         edtTitle = findViewById(R.id.EdtTitle);
         fileName = findViewById(R.id.fileName);
         edtDesc =  findViewById(R.id.EdtDesc);
@@ -73,34 +67,22 @@ public class UploadActivity extends AppCompatActivity {
         productPrice = findViewById(R.id.editTextNumberDecimal);
         agroAppRepo = AgroAppRepo.getInstanceOfAgroApp();
 
-        uploadBtn = findViewById(R.id.button2);
-        cancelBtn = findViewById(R.id.button3);
+        Button uploadBtn = findViewById(R.id.button2);
+        Button cancelBtn = findViewById(R.id.button3);
 
-        uploadBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (mUploads != null && mUploads.isInProgress()) {
-                    Toast.makeText(UploadActivity.this, "An upload is ongoing", Toast.LENGTH_LONG).show();
-                    return;
-                }
-               uploadDetails();
+        uploadBtn.setOnClickListener(view -> {
+            if (mUploads != null && mUploads.isInProgress()) {
+                Toast.makeText(UploadActivity.this, "An upload is ongoing", Toast.LENGTH_LONG).show();
+                return;
             }
+           uploadDetails();
         });
 
 
-        choBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                chooseImage();
-            }
-        });
-
-        cancelBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(UploadActivity.this,ProfileFragment.class);
-                startActivity(intent);
-            }
+        choBtn.setOnClickListener(view -> chooseImage());
+        cancelBtn.setOnClickListener(view -> {
+            Intent intent = new Intent(UploadActivity.this,ProfileFragment.class);
+            startActivity(intent);
         });
 
     }
@@ -108,7 +90,10 @@ public class UploadActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if(requestCode == IMAGE_VALUE && resultCode == Activity.RESULT_OK && data != null && data.getData() != null){
+        if(requestCode == IMAGE_VALUE
+                && resultCode == Activity.RESULT_OK
+                && data != null
+                && data.getData() != null){
             mImageUri = data.getData();
             fileName.setText(mImageUri.toString());
         }
@@ -134,67 +119,45 @@ public class UploadActivity extends AppCompatActivity {
         if(mImageUri != null){
             final StorageReference fileref = mStorageRef.child(System.currentTimeMillis() + "." + getFileExtension(mImageUri));
             mUploads = fileref.putFile(mImageUri)
-                    .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
-                        @Override
-                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    .addOnSuccessListener(taskSnapshot -> {
 
-                            Handler hand = new Handler();
-                            hand.postDelayed(new Runnable() {
-                                @Override
-                                public void run() {
-                                    bar.setProgress(0);
+                        Handler hand = new Handler();
+                        hand.postDelayed(() -> bar.setProgress(0),500);
+
+                        fileref.getDownloadUrl().addOnSuccessListener(uri -> {
+                            Toast.makeText(UploadActivity.this,"successfully uploaded",Toast.LENGTH_SHORT).show();
+                            final String tit = edtTitle.getText().toString().trim();
+                            String desc = edtDesc.getText().toString().trim();
+                            String locate = location.getText().toString();
+                            double price = Double.parseDouble(productPrice.getText().toString().trim());
+                            final FarmProduct product = new FarmProduct(desc);
+
+                            AgroAppRepo.getInstanceOfAgroApp().fetchUser(basuser -> {
+                                Log.d("User",""+basuser);
+                                long millis=System.currentTimeMillis();
+                                SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy/dd/MM hh:mm:ss", Locale.getDefault());
+                                dateFormat.setTimeZone(TimeZone.getTimeZone("UTC"));
+                                String utcTime = dateFormat.format(millis);
+                                user = basuser;
+                                product.setTitle(tit);
+                                product.setPrice(price);
+                                product.setUser(user);
+                                product.setImage(uri.toString());
+                                product.setDate(utcTime);
+                                if(product.getLocation() != null){
+                                    product.setLocation(locate);
                                 }
-                            },500);
 
-                            fileref.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
-                                @Override
-                                public void onSuccess(final Uri uri) {
-                                    Toast.makeText(UploadActivity.this,"successfully uploaded",Toast.LENGTH_SHORT).show();
-                                    final String tit = edtTitle.getText().toString().trim();
-                                    String desc = edtDesc.getText().toString().trim();
-                                    String locate = location.getText().toString();
-                                    double price = Double.parseDouble(productPrice.getText().toString().trim());
-                                    final FarmProduct product = new FarmProduct(desc);
-
-                                    AgroAppRepo.getInstanceOfAgroApp().fetchUser(new AgroAppRepo.FireBaseCallbackUser() {
-                                        @Override
-                                        public void fireBaseUser(User basuser) {
-                                            Log.d("User",""+basuser);
-                                            long millis=System.currentTimeMillis();
-                                            SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy/dd/MM hh:mm:ss", Locale.getDefault());
-                                            dateFormat.setTimeZone(TimeZone.getTimeZone("UTC"));
-                                            String utcTime = dateFormat.format(millis);
-                                            user = basuser;
-                                            product.setTitle(tit);
-                                            product.setPrice(price);
-                                            product.setUser(user);
-                                            product.setImage(uri.toString());
-                                            product.setDate(utcTime);
-                                            if(product.getLocation() != null){
-                                                product.setLocation(locate);
-                                            }
-
-                                            agroAppRepo.uploadProduct(product,UploadActivity.this);
-                                        }
-                                    });
-
-                                }
+                                Log.d("Uploading","Working fine");
+                                agroAppRepo.uploadProduct(product,UploadActivity.this);
                             });
-                        }
-                    })
-                    .addOnFailureListener(new OnFailureListener() {
-                        @Override
-                        public void onFailure(@NonNull Exception e) {
 
-                            Toast.makeText(UploadActivity.this,e.toString(),Toast.LENGTH_LONG).show();
-                        }
+                        });
                     })
-                    .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
-                        @Override
-                        public void onProgress(@NonNull UploadTask.TaskSnapshot snapshot) {
-                            double progress = (100.00 * snapshot.getBytesTransferred()/snapshot.getTotalByteCount());
-                            bar.setProgress( (int) progress);
-                        }
+                    .addOnFailureListener(e -> Toast.makeText(UploadActivity.this,e.toString(),Toast.LENGTH_LONG).show())
+                    .addOnProgressListener(snapshot -> {
+                        double progress = (100.00 * snapshot.getBytesTransferred()/snapshot.getTotalByteCount());
+                        bar.setProgress( (int) progress);
                     });
 
         }else{
